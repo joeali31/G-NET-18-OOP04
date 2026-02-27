efb9d93271ec62d2a36d3e6ba474416fac5a89bd
joeali31 <ya9415565@gmail.com>
2026-02-27 05:33:35 -0800

    question01

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Part 01 : Theoretical Questions
+            /* Q1 
+             * Static Binding : The method call is decided at compile time (before the program runs)
+             * Dynamic Binding : The method call is decided at runtime (while the program is running
+             */
+
+            /* Q2 
+             * Method Overloading : Method overloading means having multiple methods with the same name in the same class, but with different parameters (number or type)
+             * Method Overriding : Method overriding means a child class provides its own version of a method defined in the parent class
+             */
+
+            /* Q3 
+             * virtual : This method CAN be overridden by child classes.
+             */
+            #endregion
         }
     }
 }
